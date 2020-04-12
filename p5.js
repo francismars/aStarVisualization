41763364de96ad543d385151a612836501051e4e
francismars <11526661+francismars@users.noreply.github.com>
2020-04-12 05:49:02 +0100

    Add files via upload

--- p5.js
+++ p5.js
@@ -1,35 +1,46 @@
+const WIDTH = 800
+const HEIGHT = 800
+const CELLSIZE = 20
+const RANGE = CELLSIZE/2;
+const BLUEGREEN = [13, 152, 186]
+const BLUEPURPLE = [54.1, 16.9, 88.6]
+const YELLOWORANGE = [255, 174, 66]
+const REDORANGE = [255, 83, 73]
+let setInicio = false;
+let setEnd = false;
+let matriz = []
+let start = []
+let goal = []
+let dividorsX=[]
+let dividorsY=[]
+let casasOcupadas = {}
+casasOcupadas[(CELLSIZE+RANGE)+"x"+(CELLSIZE+RANGE)]="inicio";
+casasOcupadas[(WIDTH-CELLSIZE-RANGE)+"x"+(HEIGHT-CELLSIZE-RANGE)]="fim";
+
 function setup() {
-  //console.log(windowWidth, windowHeight)
-  cnv = createCanvas(800, 1000);
-  background(220);
-  noStroke();
-  rectMode(CENTER);
-  buttonFind = createButton('Find Path');
-  buttonFind.position(200,850);
-  buttonFind.mousePressed(pathFind);
-  buttonClear = createButton('Generate Maze');
-  buttonClear.position(300,850);
-  buttonClear.mousePressed(mazeAlg);
-  buttonClear = createButton('Clear Obstacules');
-  buttonClear.position(450,850);
-  buttonClear.mousePressed(clearObs);
-  buttonClear = createButton('Clear Path');
-  buttonClear.position(600,850);
-  buttonClear.mousePressed(clearPath);
+	cnv = createCanvas(WIDTH, HEIGHT);
+	background(220);
+	noStroke();
+	rectMode(CENTER);
+
+	buttonMaze = createButton('Generate Maze');
+	buttonMaze.position(175,835);
+	buttonMaze.mousePressed(mazeAlg);
+
+	buttonFind = createButton('Find Path');
+	buttonFind.position(290,835);
+	buttonFind.mousePressed(pathFind);
+
+	buttonClearObs = createButton('Clear Obstacules');
+	buttonClearObs.position(370,835);
+	buttonClearObs.mousePressed(clearObs);
+
+	buttonClearPath = createButton('Clear Path');
+	buttonClearPath.position(495,835);
+	buttonClearPath.mousePressed(clearPath);
 }
 
-casasOcupadas = {}
-casasOcupadas[30+"x"+30]="inicio";
-casasOcupadas[770+"x"+790]="fim";
-
-setInicio = false;
-setEnd = false;
-
-matriz = []
-start = []
-goal = []
-
-
+// Clear All Obstacules
 function clearObs() {
 	for (i in casasOcupadas){
 		if(casasOcupadas[i]=="ocupada"){
@@ -38,6 +49,7 @@ function clearObs() {
 	}
 }
 
+// Clear Path
 function clearPath() {
 	for (i in casasOcupadas){
 		if(casasOcupadas[i]=="caminho"){
@@ -48,51 +60,47 @@ function clearPath() {
 
 
 function mazeAlg() {
-	for (var i = 10; i < (height-200)+20; i += 20) {
-		for (var j = 10; j < width + 20; j += 20) {
-			if(i==10 || j==10 || i==height-210 || j==width+10){
+	clearObs()
+	clearPath()
+	for (var i = RANGE; i < (height)+ RANGE; i += CELLSIZE) {
+		for (var j = RANGE; j < width + RANGE; j += CELLSIZE) {
+			if(i==RANGE || j==RANGE || i==height-RANGE || j==width-RANGE){
 				casasOcupadas[i+"x"+j] = "ocupada"			
 			}			
 		}	
 	}
-	maze2(10,10,(height-210),(width+10))	
+	maze2(RANGE,RANGE,(height-RANGE),(width-RANGE))	
 }
 
-
-dividorsX=[]
-dividorsY=[]
 function maze2(x,y,h,w){
-	dividorX =(Math.floor(Math.random() * ((w-20)/20 - (x+40)/20)) + (x+40)/20) * 20
-	dividorY=(Math.floor(Math.random() * ((h-20)/20 - (y+40)/20)) + (y+40)/20) * 20
+	dividorX =(Math.floor(Math.random() * ((w-CELLSIZE)/CELLSIZE - (x+40)/CELLSIZE)) + (x+40)/CELLSIZE) * CELLSIZE
+	dividorY=(Math.floor(Math.random() * ((h-CELLSIZE)/CELLSIZE - (y+40)/CELLSIZE)) + (y+40)/CELLSIZE) * CELLSIZE
 	dividorsX.push(dividorX)
 	dividorsY.push(dividorY)
 	passagem11=-1
 	passagem12=-1
 	
 	while(passagem11<0 || passagem12<0 || dividorsY.includes(passagem11) || dividorsY.includes(passagem12)){
-		passagem11=(Math.floor(Math.random() * ((dividorY-20)/20 - (x+40)/20)) + (x+40)/20) * 20
-		passagem12=(Math.floor(Math.random() * ((w-20)/20 - (dividorY+40)/20)) + (dividorY+40)/20) * 20		
+		passagem11=(Math.floor(Math.random() * ((dividorY-CELLSIZE)/CELLSIZE - (x+40)/CELLSIZE)) + (x+40)/CELLSIZE) * CELLSIZE
+		passagem12=(Math.floor(Math.random() * ((w-CELLSIZE)/CELLSIZE - (dividorY+40)/CELLSIZE)) + (dividorY+40)/CELLSIZE) * CELLSIZE		
 	}
 	passagem21=-1
 	passagem22=-1
 	while(passagem21<0 || passagem22<0 || dividorsX.includes(passagem21) || dividorsX.includes(passagem22)){
-		passagem21=(Math.floor(Math.random() * ((dividorX-20)/20 - (y+40)/20)) + (y+40)/20) * 20
-		passagem22=(Math.floor(Math.random() * ((h-20)/20 - (dividorX+40)/20)) + (dividorX+40)/20) * 20	
+		passagem21=(Math.floor(Math.random() * ((dividorX-CELLSIZE)/CELLSIZE - (y+40)/CELLSIZE)) + (y+40)/CELLSIZE) * CELLSIZE
+		passagem22=(Math.floor(Math.random() * ((h-CELLSIZE)/CELLSIZE - (dividorX+40)/CELLSIZE)) + (dividorX+40)/CELLSIZE) * CELLSIZE	
+	}	
+	for (var j = RANGE; j < width + RANGE; j += CELLSIZE) {
+		casasOcupadas[j+"x"+dividorX] = "ocupada"						
 	}
+	delete casasOcupadas[passagem11+"x"+dividorX]
+	delete casasOcupadas[passagem12+"x"+dividorX]
+	for (var i = RANGE; i < (height)+ RANGE; i += CELLSIZE) {
+		casasOcupadas[dividorY+"x"+i] = "ocupada"						
+	}
+	delete casasOcupadas[dividorY+"x"+passagem21]
+	delete casasOcupadas[dividorY+"x"+passagem22]
 
-	
-	for (var j = 10; j < width + 20; j += 20) {
-			casasOcupadas[j+"x"+dividorX] = "ocupada"						
-	}
-	casasOcupadas[passagem11+"x"+dividorX] = "vazio"
-	casasOcupadas[passagem12+"x"+dividorX] = "vazio"
-	for (var i = 10; i < (height-200)+20; i += 20) {
-			casasOcupadas[dividorY+"x"+i] = "ocupada"						
-	}
-	casasOcupadas[dividorY+"x"+passagem21] = "vazio"
-	casasOcupadas[dividorY+"x"+passagem22] = "vazio"
-
-	console.log(x,y,h,w,dividorX,dividorY)
 	// Segundo Quadrante
 	if(dividorX-x>80 && dividorY-y>80){
 		maze2(x,y,dividorX,dividorY)
@@ -112,57 +120,48 @@ function maze2(x,y,h,w){
 }
 
 
-
 function pathFind() {
 	clearPath()
-	for (var i = 10; i < (height-200) + 20; i += 20) {
-		matriz[(i-10)/20] = []
-		for (var j = 10; j < width + 20; j += 20) {
-			matriz[(i-10)/20][(j-10)/20] = "vazio"
+	for (var i = RANGE; i < (height) + RANGE; i += CELLSIZE) {
+		matriz[(i-RANGE)/CELLSIZE] = []
+		for (var j = RANGE; j < width + RANGE; j += CELLSIZE) {
+			matriz[(i-RANGE)/CELLSIZE][(j-RANGE)/CELLSIZE] = "vazio"
 		}
 	}
+	
+	// Generate Path Finding Grid
 	for (XxY in casasOcupadas){
 		coord = XxY.split("x");
 		if(casasOcupadas[XxY]=="inicio"){
-			matriz[(coord[0]-10)/20][(coord[1]-10)/20] = "inicio"
-			start[0] = (coord[0]-10)/20
-			start[1] = (coord[1]-10)/20
+			matriz[(coord[0]-RANGE)/CELLSIZE][(coord[1]-RANGE)/CELLSIZE] = "inicio"
+			start[0] = (coord[0]-RANGE)/CELLSIZE
+			start[1] = (coord[1]-RANGE)/CELLSIZE
 		}
-		if(casasOcupadas[XxY]=="fim"){
-			matriz[(coord[0]-10)/20][(coord[1]-10)/20] = "fim"
-			goal[0] = (coord[0]-10)/20
-			goal[1] = (coord[1]-10)/20
+		else if(casasOcupadas[XxY]=="fim"){
+			matriz[(coord[0]-RANGE)/CELLSIZE][(coord[1]-RANGE)/CELLSIZE] = "fim"
+			goal[0] = (coord[0]-RANGE)/CELLSIZE
+			goal[1] = (coord[1]-RANGE)/CELLSIZE
 		}
-		if(casasOcupadas[XxY]=="ocupada"){
-			matriz[(coord[0]-10)/20][(coord[1]-10)/20] = "ocupada"
+		else if(casasOcupadas[XxY]=="ocupada"){
+			matriz[(coord[0]-RANGE)/CELLSIZE][(coord[1]-RANGE)/CELLSIZE] = "ocupada"
 		}
 	}
-	//console.log(matriz)
+	
 	foundPath = aStar()
-/* 	for(XxY in foundPath){
-		//console.log(((foundPath[XxY][0]*20)+10)+"x"+((foundPath[XxY][1]*20)+10))
-		
-	} */
-	//console.log(foundPath.length)
-	//console.log(((foundPath[0][0]*20)+10)+"x"+((foundPath[0][1]*20)+10))
+
 	for(i=0;i<foundPath.length;i++){
-		//console.log(((foundPath[i][0]*20)+10)+"x"+((foundPath[i][1]*20)+10))
-		casasOcupadas[((foundPath[i][0]*20)+10)+"x"+((foundPath[i][1]*20)+10)]="caminho"
+		casasOcupadas[((foundPath[i][0]*CELLSIZE)+RANGE)+"x"+((foundPath[i][1]*CELLSIZE)+RANGE)]="caminho"
 	}
 }
 
 function reconstruct_path(cameFrom, current){
     total_path = []
     while(cameFrom[current]!=start){
-		//console.log(current)
-		//console.log(cameFrom[current])
 		current = cameFrom[current]
         total_path.unshift(current)	
 	}
-	//console.log(total_path)
     return total_path	  
 }
-
 	
 function aStar(){
 	openSet = []
@@ -174,18 +173,42 @@ function aStar(){
 	fScore = {}
 	fScore[start] = dist(start[0], goal[0], start[1], goal[1])
 	while(openSet.length>0){
-		//console.log(matriz.length, matriz[0].length)
 		current = openSet.shift()
-		//console.log(current)
-		//console.log(goal)
 		if(current[0]==goal[0] && current[1]==goal[1]){
 			return reconstruct_path(cameFrom, current)
 		}
 		closeSet.push(current)
 		
+		for(i=-1;i<=1;i++){
+			for(j=-1;j<=1;j++){
+				if(current[0]+i>=0 && current[1]+j>=0 && 
+					current[0]+i<matriz.length && current[1]+j<matriz[0].length &&
+					matriz[current[0]+i][current[1]+j]!="ocupada"){
+					
+					tentative_gScore = gScore[current] + 1
+					neighbor=[]
+					neighbor[0]=current[0]+i
+					neighbor[1]=current[1]+j
+					if(!gScore[neighbor]){
+						gScore[neighbor]=99999;
+						
+					}				
+					if(tentative_gScore < gScore[neighbor]){
+						cameFrom[neighbor] = current
+						gScore[neighbor] = tentative_gScore
+						fScore[neighbor] = gScore[neighbor] + dist(neighbor[0], goal[0], neighbor[1], goal[1])
+						if(!closeSet.includes(neighbor)){
+							openSet.push(neighbor)
+
+						}
+											
+					}		
+					
+				}
+			}
+		}
 		
-		
-		// vizinho esquerda
+/* 		// vizinho esquerda
 		if(current[0]>0 && matriz[current[0]-1][current[1]]!="ocupada"){
 			tentative_gScore = gScore[current] + 1
 			neighbor=[]
@@ -259,148 +282,108 @@ function aStar(){
                 fScore[neighbor] = gScore[neighbor] + dist(neighbor[0], goal[0], neighbor[1], goal[1])
                 if(!closeSet.includes(neighbor)){
 					openSet.push(neighbor)
-				}
-                    				
+				}                    				
 			}		
-		}		
-		//return "falhou"
-		
+		}	 */		
 	}
-
+	return []
 }
-
-
-
-
-
 
 function draw() {
-  var lightBlue = "black";
-  background(255);
-  for (var i = 10; i < (height-200) + 20; i += 20) {
-    // draw one line of 20 rectangles across the x-axis
-    for (var j = 10; j < width + 20; j += 20) {
-      var lightBlue = "black";
-      var range = 10;
-      stroke(0, 0, 255);
-      if (i > (mouseX - range) && i < (mouseX + range) && j > (mouseY - range) && j < (mouseY + range)) {
-		if (setInicio == false & setEnd == false){
-			if (mouseIsPressed) {
-				if (mouseButton === LEFT && (!casasOcupadas[i+"x"+j] || casasOcupadas[i+"x"+j]=="caminho")) {
-				  casasOcupadas[i+"x"+j]="ocupada";
-				  //console.log(i,j);
+	background(255);
+	for (var i = RANGE; i < height + RANGE; i += CELLSIZE) {
+		for (var j = RANGE; j < width + RANGE; j += CELLSIZE) {
+		  // If mouse over
+			if (i > (mouseX - RANGE) && i < (mouseX + RANGE) && j > (mouseY - RANGE) && j < (mouseY + RANGE)) {
+				if (setInicio == false & setEnd == false){
+					if (mouseIsPressed) {
+						if (mouseButton === LEFT && (!casasOcupadas[i+"x"+j] || casasOcupadas[i+"x"+j]=="caminho")) {
+							casasOcupadas[i+"x"+j]="ocupada";
+						}
+						else if (mouseButton === LEFT && casasOcupadas[i+"x"+j]=="inicio") {
+							delete casasOcupadas[i+"x"+j];
+							setInicio = true;
+						}
+						else if (mouseButton === LEFT && casasOcupadas[i+"x"+j]=="fim") {
+							delete casasOcupadas[i+"x"+j];
+							setEnd = true;
+						}	
+						else if (mouseButton === RIGHT && casasOcupadas[i+"x"+j]=="ocupada") {
+							delete casasOcupadas[i+"x"+j];
+						}				
+					}
+					stroke(0);
+					if(casasOcupadas[i+"x"+j]=="inicio"){				
+						fill(255)
+						rect(i, j, CELLSIZE, CELLSIZE);
+						textSize(18);
+						fill(BLUEGREEN);
+						text('S', i-6, j+6);			
+					}
+					else if(casasOcupadas[i+"x"+j]=="fim"){
+						fill(255)
+						rect(i, j, CELLSIZE, CELLSIZE);
+						textSize(18);
+						fill(BLUEPURPLE);
+						text('F', i-6, j+6);
+					} else{
+						fill(123);
+						rect(i, j, CELLSIZE, CELLSIZE);					
+					}					
+				} else if (setInicio == true){
+					if (mouseIsPressed) {
+						if (mouseButton === LEFT && !casasOcupadas[i+"x"+j]) {
+							  casasOcupadas[i+"x"+j]="inicio";
+							  setInicio = false;
+						}
+					}
+					fill(BLUEGREEN);
+					rect(i,j,CELLSIZE,CELLSIZE);
+					textSize(18);
+					fill(0);
+					text('S', i-6, j+6);			
+				} else if (setEnd == true){
+					if (mouseIsPressed) {
+						if (mouseButton === LEFT && !casasOcupadas[i+"x"+j]) {
+							  casasOcupadas[i+"x"+j]="fim";
+							  setEnd = false;
+						}
+					}
+					fill(BLUEPURPLE);
+					rect(i,j,CELLSIZE,CELLSIZE);
+					textSize(18);
+					fill(0);
+					text('F', i-6, j+6);			
 				}
-				if (mouseButton === RIGHT && casasOcupadas[i+"x"+j]=="ocupada") {
-				  delete casasOcupadas[i+"x"+j];
-				  //console.log(i,j);
+			} // If mouse not over
+			else {
+				stroke(0);
+				if(casasOcupadas[i+"x"+j]=="ocupada"){
+					fill(REDORANGE)
+					rect(i, j, CELLSIZE, CELLSIZE);
 				}
-				if (mouseButton === LEFT && casasOcupadas[i+"x"+j]=="inicio") {
-				  delete casasOcupadas[i+"x"+j];
-				  setInicio = true;
-				  //console.log(setInicio)
+				else if(casasOcupadas[i+"x"+j]=="caminho"){
+					fill(YELLOWORANGE)
+					rect(i, j, CELLSIZE, CELLSIZE);
+				}		
+				else if(casasOcupadas[i+"x"+j]=="inicio"){
+					fill(BLUEGREEN)
+					rect(i, j, CELLSIZE, CELLSIZE);
+					textSize(18);
+					fill(255);
+					text('S', i-6, j+6);			
 				}
-				if (mouseButton === LEFT && casasOcupadas[i+"x"+j]=="fim") {
-				  delete casasOcupadas[i+"x"+j];
-				  setEnd = true;
-				  //console.log(setInicio)
-				}				
+				else if(casasOcupadas[i+"x"+j]=="fim"){
+					fill(BLUEPURPLE)
+					rect(i, j, CELLSIZE, CELLSIZE);
+					textSize(18);
+					fill(255);
+					text('F', i-6, j+6);
+				} else {
+					noFill();
+					rect(i, j, CELLSIZE, CELLSIZE);
+				}
 			}
-			if(casasOcupadas[i+"x"+j]=="inicio"){
-				noFill();
-				stroke(lightBlue);
-				fill('black')
-				rect(i, j, 20, 20);
-				textSize(18);
-				fill("green");
-				text('S', i-6, j+6);			
-			}
-			else if(casasOcupadas[i+"x"+j]=="fim"){
-				noFill();
-				stroke(lightBlue);
-				fill('black')
-				rect(i, j, 20, 20);
-				textSize(18);
-				fill("blue");
-				text('F', i-6, j+6);
-			} else{
-				noStroke();
-				fill("black");
-				rect(i,j,20,20);
-				stroke(lightBlue);
-				rect(i, j, 20, 20);					
-			}					
-		} else if (setInicio == true){
-			if (mouseIsPressed) {
-				if (mouseButton === LEFT && !casasOcupadas[i+"x"+j]) {
-					  casasOcupadas[i+"x"+j]="inicio";
-					  //console.log(i,j);
-					  setInicio = false;
-				}
-			}
-			noStroke();
-			fill("green");
-			rect(i,j,20,20);
-			stroke(lightBlue);
-			rect(i, j, 20, 20);
-			textSize(18);
-			fill("black");
-			text('S', i-6, j+6);			
-		} else if (setEnd == true){
-			if (mouseIsPressed) {
-				if (mouseButton === LEFT && !casasOcupadas[i+"x"+j]) {
-					  casasOcupadas[i+"x"+j]="fim";
-					  //console.log(i,j);
-					  setEnd = false;
-				}
-			}
-			noStroke();
-			fill("blue");
-			rect(i,j,20,20);
-			stroke(lightBlue);
-			rect(i, j, 20, 20);
-			textSize(18);
-			fill("white");
-			text('F', i-6, j+6);			
 		}
-      } else {
-		if(casasOcupadas[i+"x"+j]=="ocupada"){
-			noFill();
-			stroke(lightBlue);
-			fill('red')
-			rect(i, j, 20, 20);
-		}
-		else if(casasOcupadas[i+"x"+j]=="caminho"){
-			noFill();
-			stroke(lightBlue);
-			fill('yellow')
-			rect(i, j, 20, 20);
-		}		
-		else if(casasOcupadas[i+"x"+j]=="inicio"){
-			noFill();
-			stroke(lightBlue);
-			fill('green')
-			rect(i, j, 20, 20);
-			textSize(18);
-			fill("black");
-			text('S', i-6, j+6);			
-		}
-		else if(casasOcupadas[i+"x"+j]=="fim"){
-			noFill();
-			stroke(lightBlue);
-			fill('blue')
-			rect(i, j, 20, 20);
-			textSize(18);
-			fill("black");
-			text('F', i-6, j+6);
-		} else {
-			noFill();
-			stroke(lightBlue);
-			rect(i, j, 20, 20);
-		}
-
-      }
-    }
-  }
-  //console.log(casasOcupadas);
+	}
 }
-
